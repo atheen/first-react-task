fc59f6de072f574b56873126ba99185a5eb8462f
atheen <aatheen.ds@gmail.com>
2020-09-29 16:28:06 +0300

    fixed the style

--- src/App.js
+++ src/App.js
@@ -2,25 +2,66 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//data
+import items from './items.js'
+
+//styles
+import styles from './styles.js'
+
+
 function App() {
+  const itemsList = items.map((item) => (
+    <div style={styles.item} key={item.id}>
+      <img style={styles.itemImage} alt={item.name} src={item.image}/>
+      <p style={styles.text}>{item.name}</p>
+      <p style={styles.text}>{item.price} SR</p>
+    </div>
+  ));
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1 style={styles.text}>WELCOME TO THE APPLE STORE</h1>
+      <p style={styles.text}>where you can buy any apple product!</p>
+      <img src="https://www.apple.com/ac/structured-data/images/open_graph_logo.png?201809240543" style={styles.logoImage} alt="logo" />
+      <hr/>
+        {itemsList}
       </header>
     </div>
   );
 }
+
+
+
+
+
+
+// function App() {
+//   const itemList = items.map((item) => (
+//     <div style={styles.item}>
+//       <img style={styles.itemImage} alt={item.name} src={item.image}/>
+//       <p style={styles.text}>{item.name}</p>
+//       <p style={styles.text}>{item.price} SR</p>
+//     </div>
+//   ));
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//         {itemList}
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/items.js
@@ -0,0 +1,32 @@
+const items = [
+  {
+    id: "1",
+    name: "Macbook",
+    price: 5000,
+    image:
+      "https://eddy.com.sa/25961-large_default/13-inch-macbook-air-16ghz-dual-core-intel-core-i5-256gb.jpg",
+  },
+  {
+    id: "2",
+    name: "iPhone",
+    price: 4000,
+    image:
+      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone11-select-2019-family?wid=882&hei=1058&fmt=jpeg&qlt=80&op_usm=0.5,0.5&.v=1567022175704",
+  },
+  {
+    id: "3",
+    name: "Apple Watch",
+    price: 2000,
+    image:
+      "https://www.apple.com/newsroom/images/product/os/watchos/standard/Apple-watch-watchos7_06222020_LP_hero.jpg.og.jpg",
+  },
+  {
+    id: "4",
+    name: "iMac",
+    price: 10000,
+    image:
+      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/imac-215-selection-hero-201706?wid=892&hei=820&&qlt=80&.v=1570231130149",
+  },
+];
+
+export default items;
